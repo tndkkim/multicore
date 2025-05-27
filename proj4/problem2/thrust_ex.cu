5bd087620a05133608663eb23c3843e4dcde0745
tndkkim <sua104204@gmail.com>
2025-05-27 14:32:53 +0900

    final

--- proj4/problem2/thrust_ex.cu
+++ proj4/problem2/thrust_ex.cu
@@ -9,7 +9,6 @@
 #include <time.h>
 #include <cuda_runtime.h>
 
-// Function object for computing 4/(1+x^2) at each step
 struct pi_functor {
     const double step;
 
@@ -23,36 +22,30 @@ struct pi_functor {
 };
 
 int main() {
-    const long num_steps = 1000000000L;  // 1 billion steps
-    const double step = 1.0 / (double)num_steps;
+    const long num_steps = 1000000000L;
+    const double step = 1.0 / (double)num_steps; //width
 
     printf("Thrust Pi Calculation Started...\n");
     printf("Number of steps: %ld\n", num_steps);
 
-    // Start timing
+    // calculate execution time
     clock_t start_time = clock();
-
-    // Create counting iterator from 0 to num_steps-1
+    // make counting iterator 0 ~ (num steps -1)
     thrust::counting_iterator<long> first(0);
     thrust::counting_iterator<long> last = first + num_steps;
-
-    // Create transform iterator that applies pi_functor to each index
+    //make transform iterator that apply pi functor to each index
     thrust::transform_iterator<pi_functor, thrust::counting_iterator<long>>
         transform_first(first, pi_functor(step));
     thrust::transform_iterator<pi_functor, thrust::counting_iterator<long>>
         transform_last(last, pi_functor(step));
-
-    // Sum all the transformed values using thrust::reduce
+    // add all transformed values
     double sum = thrust::reduce(transform_first, transform_last, 0.0, thrust::plus<double>());
 
-    // Calculate pi
     double pi = step * sum;
 
-    // End timing
     clock_t end_time = clock();
     double elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
 
-    // Print results
     printf("Execution Time : %.10lfsec\n", elapsed_time);
     printf("pi=%.10lf\n", pi);
 
